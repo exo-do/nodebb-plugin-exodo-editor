0d7ce8a90481822de9266234ee823fbdfdce92fd
xusoo <xusoo@users.noreply.github.com>
2015-01-28 19:52:49 +0100

    Añadir algunos botones extra y mejoras en el parser
    Copiado a partir del plugin buttons-galore

---
+++ library.js
@@ -0,0 +1,54 @@
+"use strict";
+
+var plugin = {};
+
+plugin.parse = function (data, callback) {
+
+	if (data && typeof data === 'string') {
+		//preview
+		data = parser(data);
+	} else if (data.postData && data.postData.content) {
+		//post
+		data.postData.content = parser(data.postData.content);
+	} else if (data.userData && data.userData.signature) {
+		//signature
+		data.userData.signature = parser(data.userData.signature);
+	}
+
+	callback(null, data);
+};
+
+function parser(data) {
+	console.log(data)
+	//create a variable to capture code content
+	var codesTag = [];
+	//replace all codes tags by a var we can use in a regex later
+	data = data.replace(/(?:<pre>.*?<\/pre>|<code>.*?<\/code>)/g, function (match) {
+		codesTag.push(match);
+		return '__CODE__';
+	});
+	//do the replace on the whole
+	data = data
+		//Handle line breaks inside a paragraph.
+		.replace(/([^>]+)\n/g, "$1<br>")
+		//Text align left
+		.replace(/[^`]?(<(?:p|h\d)>)&lt;-([\s\S]*?)&lt;-(<\/(?:p|h\d)>)/gm, '$1<p class="text-left">$2</p>$3')
+		//Text align right
+		.replace(/[^`]?(<(?:p|h\d)>)-&gt;([\s\S]*?)-&gt;(<\/(?:p|h\d)>)/gm, '$1<p class="text-right">$2</p>$3')
+		//Text align center
+		.replace(/[^`\n]?(<(?:p|h\d)>)-&gt;([\s\S]*?)&lt;-(<\/(?:p|h\d)>)/gm, '$1<p class="text-center">$2</p>$3')
+		//Text align justify
+		.replace(/[^`]?(<(?:p|h\d)>)=&gt;([\s\S]*?)&lt;=(<\/(?:p|h\d)>)/gm, '$1<p class="text-justify">$2</p>$3')
+		//Underlined text.
+		.replace(/~([\s\S]*?)~/g, "<u>$1</u>");
+	
+	console.log(data)
+
+	//replace CODE with previously stocked code content
+	data = data.replace(/__CODE__/g, function () {
+		return codesTag.shift();
+	});
+	return data;
+};
+
+module.exports = plugin;
---
+++ public/js/buttons.js
@@ -0,0 +1,65 @@
+$('document').ready(function () {
+	require(['composer', 'composer/controls'], function (composer, controls) {
+		composer.addButton('fa fa-header', function (textarea, selectionStart, selectionEnd) {
+			writeToTextarea(controls, textarea, selectionStart, selectionEnd, '### ', '');
+		});
+		composer.addButton('fa fa-list-ol', function (textarea, selectionStart, selectionEnd) {
+			writeToTextarea(controls, textarea, selectionStart, selectionEnd, '\n1. ', '');
+		});
+		composer.addButton('fa fa-underline', function (textarea, selectionStart, selectionEnd) {
+			writeToTextarea(controls, textarea, selectionStart, selectionEnd, '~', '~');
+		});
+		composer.addButton('fa fa-strikethrough', function (textarea, selectionStart, selectionEnd) {
+			writeToTextarea(controls, textarea, selectionStart, selectionEnd, '~~', '~~', ' ');
+		});
+		composer.addButton('fa fa-file-image-o', function (textarea, selectionStart, selectionEnd) {
+			writeToTextarea(controls, textarea, selectionStart, selectionEnd, '![](', ')', 'URL');
+		});
+		composer.addButton('fa fa-code', function (textarea, selectionStart, selectionEnd) {
+			writeToTextarea(controls, textarea, selectionStart, selectionEnd, '```\n', '\n```\n');
+		});
+		composer.addButton('fa fa-quote-right', function (textarea, selectionStart, selectionEnd) {
+			writeToTextarea(controls, textarea, selectionStart, selectionEnd, '@Usuario:\n> ', '\n\n');
+		});
+		composer.addButton('fa fa-align-left', function (textarea, selectionStart, selectionEnd) {
+			writeToTextarea(controls, textarea, selectionStart, selectionEnd, '<-', '<-');
+		});
+		composer.addButton('fa fa-align-center', function (textarea, selectionStart, selectionEnd) {
+			writeToTextarea(controls, textarea, selectionStart, selectionEnd, '->', '<-');
+		});
+		composer.addButton('fa fa-align-right', function (textarea, selectionStart, selectionEnd) {
+			writeToTextarea(controls, textarea, selectionStart, selectionEnd, '->', '->');
+		});
+		composer.addButton('fa fa-align-justify', function (textarea, selectionStart, selectionEnd) {
+			writeToTextarea(controls, textarea, selectionStart, selectionEnd, '=>', '<=');
+		});
+
+	});
+
+	function writeToTextarea(controls, textarea, selectionStart, selectionEnd, tagStart, tagEnd, tagExample) {
+		if (selectionStart === selectionEnd) {
+			//No hay texto seleccionado
+			tagExample = tagExample || '';
+			controls.insertIntoTextarea(textarea, tagStart + tagExample + tagEnd);
+		} else {
+			//Englobamos el texto seleccionado
+			tagExample = '';
+			controls.wrapSelectionInTextareaWith(textarea, tagStart, tagEnd);
+		}
+		//Situamos el cursor en mitad del tag
+		controls.updateTextareaSelection(textarea, selectionStart + tagStart.length, selectionEnd + tagStart.length + tagExample.length);
+	}
+
+	$(window).on('action:composer.loaded', function (data) {
+		$('.composer:not(:last)').remove(); //Eliminamos cualquier editor anterior
+		var $toolbar = $('.formatting-bar');
+		setTimeout(function () {
+			//Reordenamos un poco la toolbar
+			$toolbar.find('span:has(.fa-italic)').after($('span:has(.fa-strikethrough)')).after($('span:has(.fa-underline)'));
+			$toolbar.find('span:has(.fa-picture-o)').after($('span:has(.fa-link)')).after($('span:has(.fa-file-image-o)'));
+			$toolbar.find('span:has(.fa-list)').after($('span:has(.fa-list-ol)'));
+			$toolbar.find('.help').appendTo($toolbar);
+		}, 0);
+	});
+
+});
